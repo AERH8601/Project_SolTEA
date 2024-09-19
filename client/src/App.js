d2b966da48371894c5eb7e899452d4f0d2a4d866
andres.rossi <andres.rossi@udea.edu.co>
2024-09-18 20:15:51 -0500

    Creación de rutas de navegación pages & components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,44 @@
+import {  createBrowserRouter, RouterProvider,} from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Write from "./pages/Write";
+import Home from "./pages/Home";
+import Single from "./pages/Single";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>
+      <Navbar/>
+      <Home/>
+      <Footer/>    
+    </div>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/single",
+    element: <Single/>,
+  },
+  {
+    path: "/write",
+    element: <Write/>,
+  },
+]);
+
 function App() {
   return (
     <div>
-      Hello world!
+      <RouterProvider router={router}/>
     </div>
   );
 }
